7b419efb6663e09b6dfe464bd10878891297ab5f
Thiago Castro <thiago.mael@gmail.com>
2014-11-27 20:36:31 -0200

    Passando a utilizar um grafo cujos nós são inteiros.
    Esse é um passo na direção da conversão dos .dot para Graph da FGL.
    Esta biblioteca possui algoritmos que podem facilitar a vida.

--- main.hs
+++ main.hs
@@ -1,27 +1,31 @@
 module Main where
 
 
+import Data.GraphViz (dotToGraph)
 import Data.GraphViz.Attributes.Complete
 import Data.GraphViz.Types
 import Data.GraphViz.Types.Generalised
 import Data.GraphViz.Commands.IO
 
-import Data.Text.Lazy (pack)
+import Data.Graph.Inductive.Graph
+import Data.Graph.Inductive.PatriciaTree  -- Instância de Graph
+
+import qualified Data.Text.Lazy as T
 
 
-invertedEdge :: DotEdge String -> DotEdge String
+invertedEdge :: DotEdge NodeType -> DotEdge NodeType
 invertedEdge edge = edge { fromNode = toNode edge
                          , toNode = fromNode edge }
 
-labeledEdge :: DotEdge String -> DotEdge String
+labeledEdge :: DotEdge NodeType -> DotEdge NodeType
 labeledEdge edge = edge { edgeAttributes = newLabel : filter isLabel (edgeAttributes edge) }
     where
         -- "\\E" é o nome da aresta (origem -> destino)
-        newLabel = (Label . StrLabel . pack) $ "\\E"
+        newLabel = (Label . StrLabel . T.pack) $ "\\E"
         isLabel = sameAttribute newLabel
 
 
-transformedStatement :: DotStatement String -> DotStatement String
+transformedStatement :: DotStatement NodeType -> DotStatement NodeType
 transformedStatement stmt = case stmt of
         DE edge -> DE (transformedEdge edge)
         otherwise -> stmt
@@ -29,13 +33,18 @@ transformedStatement stmt = case stmt of
         transformedEdge = invertedEdge . labeledEdge
 
 
-invertedGraph :: DotGraph String -> DotGraph String
+invertedGraph :: DotGraph NodeType -> DotGraph NodeType
 invertedGraph original = original {
         graphStatements = fmap transformedStatement $ graphStatements original
     }
 
 
+type NodeType = Node
+
+
 main :: IO ()
 main = do
-    graph <- readDotFile "simple.dot" :: IO(DotGraph String)
+    --graph <- readDotFile "simple.dot" :: IO(DotGraph NodeType)
+    graph <- readDotFile "simple_int.dot" :: IO(DotGraph NodeType)
+    print $ (dotToGraph graph :: Gr Attributes Attributes)
     writeDotFile "simple_out.dot" $ invertedGraph graph
