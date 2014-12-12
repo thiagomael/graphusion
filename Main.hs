d512ad6e5601998b8deb5a1ea87cede986074e90
Thiago Castro <thiago.mael@gmail.com>
2014-12-12 00:04:32 -0200

    Ligeira alteração do exemplo.

--- Main.hs
+++ Main.hs
@@ -4,11 +4,8 @@ import Data.FDTMC
 import Data.FDTMC.Parsers.Dot
 import Data.FDTMC.Printers.Dot
 
-import Data.GraphViz.Commands.IO
-
 
 main = do
-    dotGraph <- readDotFile "complete.dot"
-    print $ dotToFDTMC dotGraph
-    writeDotFile "alalao.dot" $ fdtmcToDot . (`resolve` ['f']) . dotToFDTMC $ dotGraph
+    fdtmc <- parseDotFile "complete.dot"
+    writeDotFile "alalao.dot" $ pruneUnreachableStates . (`resolve` ['f']) $ fdtmc
     return ()
